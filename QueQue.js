976c925d202a3a3890699c507f243d088fbaa634
Valentin <valentin.starck@gmail.com>
2011-05-25 13:27:25 -0300

    Va tomando forma

--- QueQue.js
+++ QueQue.js
@@ -1,9 +1,29 @@
+/**
+ * @author Valentin Starck (@aijoona)
+ * @see
+ * 
+ * QueQue is freely distributable under the terms of an MIT-style license.
+ ********************************************************************************/
 var QueQue = (function() {
-	function each(obj, fn, scope) {
+	function each(obj, fn, scope) {	
 		var i = 0, p;
-		for(p in obj) if(obj.hasOwnProperty(p)) {
-			// value, key, index, obj
-			fn.call(scope, obj[p], p, i++, obj);
+		
+		if(Object.prototype.toString.call(obj) != '[object Array]') {
+			for(p in obj) if(obj.hasOwnProperty(p)) {
+				// value, key, index, obj
+				fn.call(scope, obj[p], p, i++, obj);
+			}			
+			return;
+		} 
+		
+		if(Array.prototype.forEach) {
+			Array.prototype.forEach.call(obj, fn);
+			return;
+		}
+		
+		for(i = 0, p = obj.length; i < p; i++) {
+			// value, index, obj
+			fn.call(scope, obj[i], i, obj);
 		}
 	}
 	
@@ -14,88 +34,270 @@ var QueQue = (function() {
 		return target;
 	}
 	
-	function Constructor(config) {	
+	/**
+	 * Utility method
+	 *
+	 * 	eventize("foo") //"onFoo"
+	 *
+	 * @param {String} str
+	 * @return {String}
+	 */
+	function eventize(str) {
+		return str.replace(/^[a-z]/i, function(c) {
+			return 'on' + c.toUpperCase();
+		});
+	}
+	/**
+	 * @constructor
+	 * @param {Object} config
+	 */
+	function QueQue(config) {
+		// allow QueQue().add() syntax
+		if(!(this instanceof QueQue)) {
+			return new QueQue(config);
+		}
+	
 		this._tasks = [];
 		this._handlers = [];
 		override(this, config || {});		
 	}
 	
-	var EVENTS = {
-		'start': 'onStart',
-		'end': 'onEnd',
-		'add': 'onAdd',
-		'remove': 'onRemove',
-		'flush': 'onFlush',
-		'exception': 'onException',
-		'stepComplete': 'onStepComplete'
-	};
+	var EVENTS = [
+		'start',
+		'stop',
+		'complete',
+		'add',
+		'remove',
+		'clear',
+		'exception'
+	];
 	
-	Constructor.prototype = {
-		add: function(fn, opts) {
+	QueQue.prototype = {
+		/**
+		 * @param {Function} fn
+		 * @param {Object} opts
+		 */
+		add: function(fn, opts) {	
 			var step = override({
-			
+				async: false,
+				scope: this,
+				fn: fn
 			}, opts || {});
 			
-			this._tasks.push(step);
+			
+			this.trigger('add', [step]);
+			
+			if(step) {
+				this._tasks.push(step);
+			}
 			
 			return this;
 		},
-		remove: function(fn) {
+		/**
+		 * @param {Function} fn
+		 * @param {Number} limit
+		 */
+		remove: function(fn, limit) {
             var tasks = this._tasks;
 
-            tasks.splice(tasks.indexOf(fn), 1);
+            //tasks.splice(tasks.indexOf(fn), 1);
 
             return this;
 		},
-		step: function() {
-			var
-                    step;
+		/**
+		 * @param {Object} lastResult
+		 * @return {QueQue}
+		 */
+		step: function(lastResult) {
+			var 
+					self = this, 
+					step, 
+					proxy = {};		
+			
+			if(!this._running) {
+				return this;
+			}
 
-            this._proxy = {};
+			if(!this._tasks.length) {
+				this.trigger('complete');
+				return this.end();
+			}			
 
+			step = this._tasks.shift();
+			
+			proxy.qq = this;
+			proxy.memo = lastResult;
+
+			if(step.async) {
+				proxy.end = function(data) {
+					return self.step(data);
+				}
+			}
+			
 			try {
-				step.fn.call(step.scope || this, this._proxy);
+				proxy.memo = step.fn.call(step.scope || this, proxy);
 			} catch(e) {
 				if(step.onException) {
 					step.onException.call(this, e, this);
+				} else {
+					throw e;
 				}
 			}
-		},
-		stop: function() {
-            this._running = false;
-            return this;
-        },
-		start: function() {
-            this._running = true;
-            return this.step();
-        },
-		clear: function() {
-			this._tasks = [];
-			return this.stop();
-		},
-		on: function(event, handler) {
-			var ev = this._handlers[event] = this._handlers[event] || [];
 			
-			ev.push(handler);
+			if(!step.async) {
+				this.step(proxy.memo);		
+			}
 			
 			return this;
 		},
-		trigger: function(ev, args, scope) {
-		
+		/**
+		 * @return {QueQue}
+		 */
+		end: function() {
+            this
+				.trigger('complete')
+				.clear()
+				._running = false;			
+
+            return this;		
+		},
+		/**
+		 * @return {QueQue}
+		 */
+		stop: function() {
+			this
+				.trigger('stop')
+				._running = false;
+				
+            return this;
+        },
+		/**
+		 * @param {Object}obj
+		 * @return {QueQue}
+		 */
+		start: function(obj) {
+			this
+				.trigger('start')
+				._running = true;
+				
+            return this.step(obj);
+        },
+		/**
+		 * @return {QueQue}
+		 */
+		clear: function() {
+			this._tasks = [];
+			return this;
+		},
+		/**
+		 * @param {String} event
+		 * @param {Function} Handler
+		 * @return {QueQue}
+		 */
+		on: function(event, handler) {	
+			(this._handlers[event] = this._handlers[event] || []).push(handler);
+			
+			return this;
+		},
+		/**
+		 * @param {String} event
+		 * @param {Array} scope
+		 * @param {Object} args
+		 * @return {QueQue}
+		 */
+		trigger: function(event, args, scope) {
+			var handlers = this._handlers[event] || [];
+			
+			each(handlers, function(h) {
+				h.apply(args);
+			});
+			
 			return this;
 		}
 	};
 	
-	each(EVENTS, function(fn, event) {
-		//console.log(event, fn);
-		this[fn] = function(handler) {
-			return this.on(event, handler);
-		}
-	}, Constructor.prototype);
+	// Events shorthands 
+	// QueQue().on('complete',fn) -> QueQue().onComplete(fn)
+	each(EVENTS, function(e) {
+		QueQue.prototype[eventize(e)] = function(handler) {
+			return this.on(e, handler);
+		}		
+	});
 	
-	return Constructor;
+	return QueQue;
 })();
+//-----------------------------------------------------------------------------------------------------------------
 
-var q = new Queue;
+// Pipeline
+var q = new QueQue;
 
-q.onStepComplete(alert);
+//q.onStepComplete(alert);
+/*
+q
+
+.add(function(proxy) {
+	var value = {};
+	
+	value['s1'] = true;
+	console.log('s1 ready');
+	return value;
+})
+
+.add(function(proxy) {
+	var value = proxy.memo;
+	
+	value['s2'] = true;
+	console.log('s2 ready');
+	
+	return value;
+})
+
+.add(function(proxy) {
+	var value = proxy.memo;
+	
+	value['s3'] = true;
+	
+	setTimeout(function() {
+		console.log('s3 ready');
+		proxy.end(value);	
+	}, 1500);	
+	
+}, { async: true })
+
+.add(function(proxy) {
+	var value = proxy.memo;
+	
+	value['s4'] = true;
+	
+	console.log(value);
+	
+	return value;	
+})
+
+.start();
+*/
+//-----------------------------------------------------------------------------------------------------------------
+// Events
+
+
+
+var qq = QueQue()
+	.onComplete(function() {
+		console.log('Complete I');
+	})
+	.on('complete', function() {
+		console.log('Complete II');
+	})
+	.add(function(proxy) {
+		setTimeout(function() {
+			console.log('Ready 1');
+			proxy.end();
+		},1500);
+	}, { async: true})
+	.add(function(proxy) {
+		setTimeout(function() {
+			console.log('Ready 2');
+			proxy.end();
+		},1500);
+	}, { async: true})
+	.start();
